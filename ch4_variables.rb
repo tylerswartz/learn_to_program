5d1fa13a0747ec28cb04f666fa7cea1f92dd5c9c
Tyler Swartz <tgswartz@gmail.com>
2014-01-09 07:09:05 -0800

    chapter 4 scratchwork

---
+++ ch4_variables.rb
@@ -0,0 +1,25 @@
+# my_string = '...you can say that again'
+# puts my_string
+# puts my_string
+
+# name = 'Anya Christina Emmanuella Jenkins Harris'
+# puts 'My name is ' + name + '.'
+# puts 'Wow! ' + name
+# puts 'is a really long name!'
+
+# composer = 'Mozart'
+# puts composer + ' was "da bomb" in his day.'
+
+# composer = 'Beethoven'
+# puts 'but I prefer ' + composer + ', personally.'
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
