18e5c019a7ad1f1551791caa1e88635d3c2cdfab
Tyler Swartz <tgswartz@gmail.com>
2014-01-09 06:46:10 -0800

    chapter 3 scratchwork

---
+++ ch3_letters.rb
@@ -0,0 +1,14 @@
+# puts 'Hello, world!'
+# puts ''
+# puts 'Good-bye.'
+# puts 'I like ' + 'apple pie.'
+# puts 'blink ' * 4
+
+# puts 12 + 12
+# puts '12' + '12'
+# puts '12 + 12'
+
+puts 'You\'re swell'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
