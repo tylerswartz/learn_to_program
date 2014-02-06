fa1e9cda2744e36a9475fc765c9b0ede6e039683
Tyler Swartz <tgswartz@gmail.com>
2014-02-06 06:58:17 -0800

    ch12 update

--- ch12_birthday.rb
+++ ch12_birthday.rb
@@ -1,1 +1,20 @@
-ch12_birthday.rb
+# ch12_birthday.rb
+
+puts "Hello, what year were you born?"
+year = gets.chomp
+
+puts "and the month? (in 1-12 format please)"
+month = gets.chomp
+
+puts "and the day?"
+day = gets.chomp
+
+today = Time.new
+
+age = 1
+
+while Time.local(year.to_i + age, month, day) <= today
+  puts "Spank"
+  age = age + 1
+end
+
